3d7c9e4be6248fb0191482305592d2ff4843b9b3
Emmanuel Gonzalez <emmanuelgonzalez08.eg@gmail.com>
2025-04-17 15:29:38 -0300

    creacion de pagina 404 con su pagina de estilo

--- src/main.jsx
+++ src/main.jsx
@@ -10,8 +10,10 @@ import { Footer } from "./assets/layout/Footer";
 import Pueba from "./assets/pages/Pueba";
 import Categories from "./component/Categories";
 import "./index.css";
+import NoFound from './assets/pages/Error404'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -24,6 +26,7 @@ createRoot(document.getElementById("root")).render(
         <Route path="/cart" element={<CartPage />} />
         <Route path="/prueba" element={<Pueba />} />
         <Route path="/categories" element={<Categories />} />
+        <Route path="*" element ={<NoFound></NoFound>}></Route>
       </Routes>
       <Footer />
     </Router>
