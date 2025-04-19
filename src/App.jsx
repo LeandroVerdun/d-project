74b556bffa897225575e1c646d7fd57b1f11dd37
Santiago Emmanuel Molina <santiagoemmanuelmolina@gmail.com>
2025-04-19 15:03:53 -0300

    limpie codigo app y main de src

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-import './App.css'
-
-
-
-
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { MainPage } from "./assets/pages/main";
+import Login from "./assets/pages/Login";
+import RegistrationForm from "./assets/pages/Register";
+import CartPage from "./component/CartPage";
+import { Navbar } from "./assets/layout/navbar";
+import { Footer } from "./assets/layout/Footer";
+import Pueba from "./assets/pages/Pueba";
+import Categories from "./component/Categories";
+import NoFound from "./assets/pages/Error404";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function App() {
-  
-
   return (
-    <>
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<RegistrationForm />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/prueba" element={<Pueba />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="*" element={<NoFound />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Importar BrowserRouter y Routes
-import { MainPage } from "./assets/pages/main";
-import Login from "./assets/pages/Login";
-import RegistrationForm from "./assets/pages/Register";
-import CartPage from "./component/CartPage";
-import { Navbar } from "./assets/layout/navbar";
-import { Footer } from "./assets/layout/Footer";
-import Pueba from "./assets/pages/Pueba";
-import Categories from "./component/Categories";
+import App from "./App"; // Importa el componente App
 import "./index.css";
-import NoFound from './assets/pages/Error404'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<RegistrationForm />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/prueba" element={<Pueba />} />
-        <Route path="/categories" element={<Categories />} />
-        <Route path="*" element ={<NoFound></NoFound>}></Route>
-      </Routes>
-      <Footer />
-    </Router>
+    <App /> {/* Renderiza el componente App que contiene el enrutamiento */}
   </StrictMode>
 );
