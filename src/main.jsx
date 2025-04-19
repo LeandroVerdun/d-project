5a1784a778bbea6f44f6da2c5af12a27c9f47f3d
Santiago Emmanuel Molina <santiagoemmanuelmolina@gmail.com>
2025-04-19 15:05:38 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App"; // Importa el componente App
+import App from "./App";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App /> {/* Renderiza el componente App que contiene el enrutamiento */}
+    <App />
   </StrictMode>
 );
