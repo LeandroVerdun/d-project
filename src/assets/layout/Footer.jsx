51e65926817433a39a981e34426f84d99ca284b4
Santiago Emmanuel Molina <santiagoemmanuelmolina@gmail.com>
2025-04-25 01:11:15 -0300

    cree css de Footer para reajustes y mobile

--- src/assets/layout/Footer.jsx
+++ src/assets/layout/Footer.jsx
@@ -1,9 +1,26 @@
-// Footer.jsx
+// src/assets/layout/Footer.jsx
 import React from "react";
 import { FaFacebookF, FaTwitter, FaInstagram, FaYoutube } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import "../../css/Footer.css"; // Importa el archivo CSS
 
 export const Footer = () => {
+  const navigate = useNavigate();
+  const user = JSON.parse(localStorage.getItem("user"));
+  const isLoggedIn = !!user;
+
+  const handleProfileLink = (e) => {
+    if (!isLoggedIn) {
+      e.preventDefault();
+      navigate("/404");
+    }
+  };
+
+  const handleFollowUsLink = (e) => {
+    e.preventDefault();
+    navigate("/404");
+  };
+
   return (
     <footer className="bg-dark text-light pt-5 pb-4">
       <div className="container">
@@ -12,8 +29,11 @@ export const Footer = () => {
           <div className="col-md-3 mb-4">
             <h5 className="text-uppercase mb-3">Navigation</h5>
             <ul className="list-unstyled">
-              <li><Link to="/" className="text-light text-decoration-none">Home</Link></li>
-              <li><Link to="/movies" className="text-light text-decoration-none">Movies</Link></li>
+              <li>
+                <Link to="/" className="text-light text-decoration-none">
+                  Home
+                </Link>
+              </li>
             </ul>
           </div>
 
@@ -21,9 +41,22 @@ export const Footer = () => {
           <div className="col-md-3 mb-4">
             <h5 className="text-uppercase mb-3">Help</h5>
             <ul className="list-unstyled">
-              <li><Link to="/about-us" className="text-light text-decoration-none">About Us</Link></li>
-              <li><Link to="/terms-of-service" className="text-light text-decoration-none">Terms of Service</Link></li> {/* Actualización aquí */}
-              <li><a href="#" className="text-light text-decoration-none">Contact</a></li>
+              <li>
+                <Link
+                  to="/about-us"
+                  className="text-light text-decoration-none"
+                >
+                  About Us
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/terms-of-service"
+                  className="text-light text-decoration-none"
+                >
+                  Terms of Service
+                </Link>
+              </li>
             </ul>
           </div>
 
@@ -31,18 +64,53 @@ export const Footer = () => {
           <div className="col-md-3 mb-4">
             <h5 className="text-uppercase mb-3">Account</h5>
             <ul className="list-unstyled">
-              <li><a href="#" className="text-light text-decoration-none">My Profile</a></li>
-              <li><a href="#" className="text-light text-decoration-none">Subscriptions</a></li>
+              <li>
+                <Link
+                  to="/profile"
+                  className="text-light text-decoration-none"
+                  onClick={handleProfileLink}
+                >
+                  My Profile
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/register"
+                  className="text-light text-decoration-none"
+                >
+                  Sign Up
+                </Link>
+              </li>
             </ul>
           </div>
 
           {/* Social Media */}
           <div className="col-md-3 mb-4">
             <h5 className="text-uppercase mb-3">Follow Us</h5>
-            <div className="d-flex gap-3">
-              <a href="#" className="text-light fs-5"><FaTwitter /></a>
-              <a href="#" className="text-light fs-5"><FaInstagram /></a>
-              <a href="#" className="text-light fs-5"><FaYoutube /></a>
+            <div className="social-icons-container">
+              {" "}
+              {/* Usa la clase del CSS */}
+              <a
+                href="#"
+                className="text-light fs-5"
+                onClick={handleFollowUsLink}
+              >
+                <FaTwitter />
+              </a>
+              <a
+                href="#"
+                className="text-light fs-5"
+                onClick={handleFollowUsLink}
+              >
+                <FaInstagram />
+              </a>
+              <a
+                href="#"
+                className="text-light fs-5"
+                onClick={handleFollowUsLink}
+              >
+                <FaYoutube />
+              </a>
             </div>
           </div>
         </div>
@@ -50,7 +118,9 @@ export const Footer = () => {
         {/* Bottom Line */}
         <hr className="bg-secondary" />
         <div className="text-center">
-          <p className="mb-0">&copy; {new Date().getFullYear()} MovieApp. All rights reserved.</p>
+          <p className="mb-0">
+            &copy; {new Date().getFullYear()} MovieApp. All rights reserved.
+          </p>
         </div>
       </div>
     </footer>
