36da792aca5757265fc9475c7ded6be0959ddc00
Santiago Emmanuel Molina <santiagoemmanuelmolina@gmail.com>
2025-04-25 02:12:20 -0300

    Modificacion text about us

--- src/component/AboutUs.jsx
+++ src/component/AboutUs.jsx
@@ -1,26 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import '../css/AboutUs.css';
+import React, { useEffect, useState } from "react";
+import "../css/AboutUs.css";
 
 const collaborators = [
   {
-    name: 'Lean',
-    img: '/assets/lean.png',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    name: "Lean",
+    img: "/assets/lean.png",
+    text: "Coding courage builds the web's future.",
   },
   {
-    name: 'Santi',
-    img: '/assets/santi.png',
-    text: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    name: "Santi",
+    img: "/assets/santi.png",
+    text: "Valor in code: persistence over bugs.",
   },
   {
-    name: 'AiBroy',
-    img: '/assets/aibroy.png',
-    text: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
-  },
-  {
-    name: 'Julian',
-    img: '/assets/julian.png',
-    text: 'Duis aute irure dolor in reprehenderit in voluptate velit esse.',
+    name: "AiBroy",
+    img: "/assets/aibroy.png",
+    text: "Bravery: coding beyond the known.",
   },
 ];
 
@@ -29,7 +24,7 @@ export const AboutUs = () => {
 
   useEffect(() => {
     const timer = setInterval(() => {
-      setCurrent(prev => (prev + 1) % collaborators.length);
+      setCurrent((prev) => (prev + 1) % collaborators.length);
     }, 5000);
 
     return () => clearInterval(timer);
