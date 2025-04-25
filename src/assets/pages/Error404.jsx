3052b104c69ae14fec941b9a320b4dce3605c673
Santiago Emmanuel Molina <santiagoemmanuelmolina@gmail.com>
2025-04-25 01:18:34 -0300

    Modifique el Error 404, estilos y mobile

--- src/assets/pages/Error404.jsx
+++ src/assets/pages/Error404.jsx
@@ -1,18 +1,25 @@
-import {Link} from "react-router-dom"
-import Button from 'react-bootstrap/Button';
+// src/assets/pages/Error404.jsx
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 import "../../css/error.css";
-import error404Image from '../img/404.png';
+import error404Image from "../img/404.png";
 
-const NoFound= () =>{
-    return <>
-   <div className="body">
-    <h1 className="text-center mt-3 mb-3 pulsating">ERROR 404!</h1>
-    <div className="text-center responsive-container" >
-    <img src={error404Image} alt="Error 404 Not Found" />
-    <Button variant="dark"><Link to={"/"} style={{color:"white"}}>GO TO HOME </Link></Button>
+const NoFound = () => {
+  return (
+    <div className="body">
+      <h1 className="text-center mt-3 mb-3 pulsating">
+        ¡Oops! Página no encontrada
+      </h1>
+      <div className="responsive-container">
+        <img src={error404Image} alt="Error 404 Not Found" />
+        <Button variant="dark">
+          <Link to={"/"} style={{ color: "white", textDecoration: "none" }}>
+            Volver al inicio
+          </Link>
+        </Button>
+      </div>
     </div>
-    </div>
-    </>
-}
+  );
+};
 
-export default NoFound
+export default NoFound;
